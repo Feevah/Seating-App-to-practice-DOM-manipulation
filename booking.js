c3f1cb7dd992bc2edd17b1b3d67fd704e7eb0b4c
Feevah <cddefever@gmail.com>
2020-02-23 22:37:41 -0500

    functionality

--- booking.js
+++ booking.js
@@ -4,6 +4,34 @@ const count = document.getElementById('count');
 const total =document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
-console.log(ticketPrice);
+
+//Update Total and Count
+function updateSelectedCount () {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+
+}
+
+
+//Movie Select Event
+
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+//Seat Click event
+
+container.addEventListener('click', function(e){
+    if(e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+    }
+
+    updateSelectedCount();
+})
