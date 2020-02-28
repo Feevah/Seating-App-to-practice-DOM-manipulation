c4a1f6a3356658f421183ee1bc608289f0eb7970
Feevah <cddefever@gmail.com>
2020-02-27 21:37:37 -0500

    localStorage

--- booking.js
+++ booking.js
@@ -4,12 +4,27 @@ const count = document.getElementById('count');
 const total =document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+// populateUI();
+
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index and price 
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+};
 
 //Update Total and Count
 function updateSelectedCount () {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map((seat) => {
+        return [...seats].indexOf(seat);
+    });
+
+   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
@@ -18,10 +33,20 @@ function updateSelectedCount () {
 }
 
 
+
+
+// Get data from local storage and populate UI 
+
+// function populteUI() {
+//     const selectedSeats
+
+// }
+
 //Movie Select Event
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+ setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
